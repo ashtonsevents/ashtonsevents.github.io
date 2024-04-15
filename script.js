40eb94cd56a519ac1db672d9a653d6f225491cad
ashtons event pack <159561871+ashtonsevents@users.noreply.github.com>
2024-04-15 10:21:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function downloadFile() {
+    // Get the file URL
+    var fileUrl = "assets/Ashtons Primary Care Flyer.pdf"; // Corrected URL
+
+    // Open the file in a new tab
+    window.open(fileUrl, "_blank");
+}
