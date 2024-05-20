d8c3e86c5a135cf5855e8ea49baacf275db1fa5e
ashtons event pack <159561871+ashtonsevents@users.noreply.github.com>
2024-05-20 13:39:26 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function downloadFile() {
-    // Get the file URL
-    var fileUrl = "assets/Ashtons Primary Care Flyer.pdf"; // Corrected URL
-
-    // Open the file in a new tab
-    window.open(fileUrl, "_blank");
-}
